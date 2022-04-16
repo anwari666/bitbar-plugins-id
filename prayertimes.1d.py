3b913c0fc22260f089b6700ff02f4256d7c61731
Anwari Ilman <anwari.ilman@ctw.bmwgroup.com>
2022-04-16 06:11:24 +0100

    feature: add details for Bandung's prayer times

--- prayertimes.1d.py
+++ prayertimes.1d.py
@@ -1,53 +1,73 @@
 #!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/bin/python3
 # -*- coding: utf-8 -*-
-# Fetch prayer times according to SalahHour.com API
+
+# Fetch prayer times according to SalahHour.com API. 
+# More info regarding calculation method, etc can be found under
+# this page http://salahhour.com/index.php/api/index
 
 import urllib.request
 import json
 
-# CHANGE YOUR SETTINGS HERE
+# ------ SETTINGS -------
+# Grab your latitude and longitude from Google Map
+# by pinpointing any location, and you'll find those
+# in the url with this format `@latitude,longitude` e.g @12.345,67.123
 
-latitude=41.176181
-longitude=-8.584248
-timezone="Europe/Lisbon"
+latitude  = -6.9034443
+longitude = 107.5731168
+timezone  = "Asia/Jakarta"
+method    = 12 # 12 is from Sistem Informasi Hisab Rukyat Indonesia. 
+# ------ END SETTINGS -------
 
-font="Monaco"
 
 
-url = "http://salahhour.com/index.php/api/prayer_times?latitude={}&longitude={}" \
-      "&timezone={}&time_format=0&method=3&maghrib_rule=1&maghrib_value=4".format(latitude, longitude, timezone)
+def format_line(str):
+  return str + "| trim=false href=#"
 
-print(url)
 
-req = urllib.request.Request(url)
+def format_monospace(str):
+  font="Monaco"
+  return str + f" font={font}"
 
-print("🙏")
-print("---")
 
-resp = None
+def main():
+  url = "http://salahhour.com/index.php/api/prayer_times?latitude={}&longitude={}" \
+        "&timezone={}&time_format=0&method={}" \
+        "&maghrib_rule=1&maghrib_value=4" \
+        .format(latitude, longitude, timezone, method)
 
-try: 
-  resp = urllib.request.urlopen( req )
+  req = urllib.request.Request(url)
 
-except:
-  "tyda bisa konek :("
-
-if resp is not None:
-  result = json.loads(resp.read())['results']
-  asr = result['Asr']
-  dhuhr = result['Dhuhr']
-  maghrib = result['Maghrib']
-  isha = result['Isha']
-  fajr = result['Fajr']
-
-  print( f"Jadwal solat hari ini")
+  print("🙏")
   print("---")
-  
-  print( f"Shubuh\t\t{fajr}|trim=false font={font}" )
-  print( f"Dzuhur\t\t{dhuhr}|trim=false font={font}" )
-  print( f"Asr\t\t\t{asr}|trim=false font={font}" )
-  print( f"Maghrib\t\t{maghrib}|trim=false font={font}" )
-  print( f"Isya\t\t{isha}|trim=false font={font}" )
 
-else:
-  print( "Cannot connect to SalahHour" )
+  resp = None
+
+  try: 
+    resp = urllib.request.urlopen( req )
+
+  except:
+    "tyda bisa konek :("
+
+  if resp is not None:
+    result = json.loads(resp.read())['results']
+    asr = result['Asr']
+    dhuhr = result['Dhuhr']
+    maghrib = result['Maghrib']
+    isha = result['Isha']
+    fajr = result['Fajr']
+
+    print( format_line(f"Jadwal solat hari ini"))
+    print("---")
+    
+    print( format_monospace(format_line(f"Shubuh\t\t\t{fajr}" )))
+    print( format_monospace(format_line(f"Dzuhur\t\t\t{dhuhr}" )))
+    print( format_monospace(format_line(f"Asr\t\t\t\t{asr}" )))
+    print( format_monospace(format_line(f"Maghrib\t\t\t{maghrib}" )))
+    print( format_monospace(format_line(f"Isya\t\t\t{isha}" )))
+
+  else:
+    print( "Cannot connect to SalahHour API" )
+
+# Actually run the app
+main()
